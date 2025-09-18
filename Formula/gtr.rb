8ef88949b96086a50614045fb8e88adbb509af0d
Ryan <ryanwjackson@users.noreply.github.com>
2025-09-18 15:42:38 -0700

    gtr: bump to v0.3.0 (#22)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/gtr.rb
+++ Formula/gtr.rb
@@ -1,8 +1,8 @@
 class Gtr < Formula
   desc "Git worktree helper"
   homepage "https://github.com/ryanwjackson/gtr"
-  url "https://github.com/ryanwjackson/gtr/releases/download/v0.2.0/gtr-v0.2.0.tar.gz"
-  sha256 "37391b6f7aceb2456e9d940fbe78e2df893134c5ff3ee268733b136064492894"
+  url "https://github.com/ryanwjackson/gtr/releases/download/v0.3.0/gtr-v0.3.0.tar.gz"
+  sha256 "2d4f1735a1a7608f408e61860597a24dde605e2cde17acc4e82888ff03dd7cd6"
   license "MIT"
   head "https://github.com/ryanwjackson/gtr.git", branch: "main"
 
